e2d8be2e9b3fc96a1d8cb6cef609fb28046668aa
Fouche du Preez <fouche@limehouse.co.za>
2024-01-26 20:37:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Me:</h1>
+  <p>{{ name }} - {{ age }}</p>
+  <button @click="changeName('Zelda')">Change Name</button>
+  <br />
+  <h2>Children:</h2>
+  <Children :children="children"/>
+</template>
+<script>
+import {defineComponent, ref} from "vue"
+import Children from "@/components/Children.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {Children},
+  setup() {
+    const name = ref('John')
+    const age = ref(30)
+    const children = ref([
+      { name: 'Mary'},
+      { name: 'Jane'},
+      { name: 'Tom'},
+      { name: 'Dick'},
+      { name: 'Harry'},
+    ])
+
+    return {name, age, children}
+  },
+  methods: {
+    changeName(name) {
+      this.name = name
+    }
+  }
+})
+
+</script>
---
+++ src/components/Children.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="child in children" :key="child.name">
+        Name: {{ child.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    name: 'Children',
+    props: {
+      children: {
+        required: true,
+        type: Array
+      }
+    }
+  })
+</script>
